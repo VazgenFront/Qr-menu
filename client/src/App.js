da73ed62d8096a23c116c4f9d43080d5819992cc
DAVTYAN Vazgen <vazgen.davtyan@raiffeisen.ru>
2021-11-30 22:43:30 +0300

    add packajes, routes

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import { switchingRoutes } from "./routes";
+
+function App() {
+  return <div>{switchingRoutes()}</div>;
+}
+
+export default App;
---
+++ client/src/screens/MainPage/MainPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ExampleText } from "./MainPage.styles";
+const MainPage = () => {
+  return (
+    <div>
+      <ExampleText>Example</ExampleText>
+    </div>
+  );
+};
+
+export default MainPage;
