b2d37164048d2ce259c6d13f476965d8f44a0a28
VazgenFront <95051641+VazgenFront@users.noreply.github.com>
2022-01-23 19:16:55 +0400

    Revert "add prepush changes"

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,9 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import {
-  BrowserRouter,
-  Switch,
-  useHistory,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter, Switch, useHistory } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import ThemeContextProvider from "./context/ThemeContext";
 import { useRoutes } from "./routes";
-import { AdminAuh, AdminPage } from "./screens/index";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
@@ -29,20 +22,8 @@ function App() {
           history.location.pathname === "/" ? null : (
             <Navbar />
           )}
-          <Switch>
-            <Route
-              path="/:id/:cafeName/admin-panel/dashboard"
-              exact
-              render={() =>
-                localStorage.getItem("adminToken") ? (
-                  <AdminPage />
-                ) : (
-                  <Redirect to="/admin-panel/login" />
-                )
-              }
-            />
+          <Switch>{routes}
           </Switch>
-          <Switch>{routes}</Switch>
         </ThemeContextProvider>
       </ApolloProvider>
     </BrowserRouter>
--- client/src/screens/index.js
+++ client/src/screens/index.js
@@ -4,6 +4,5 @@ import Cafe from "./Cafe/Cafe.js";
 import MenuType from "./MenuType/MenuType.js";
 import Card from "./Card/Card.js";
 import MenuItem from "./MenuItem/MenuItem.js";
-import AdminPage from "./AdminPage/AdminPage";
 
-export { AboutUs, AdminAuh, Cafe, MenuType, Card, MenuItem, AdminPage };
+export { AboutUs, AdminAuh, Cafe, MenuType, Card, MenuItem };
